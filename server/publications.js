9282bd2555408c0e39f026cd8d13e845ccad20cc
StorytellerCZ <jxd3455@rit.edu>
2016-03-08 03:20:16 -0500

    Add check() to publication :white_check_mark:
    Or it should be removed altogether

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,4 @@
 Meteor.publish("profileFor", function(userIdOrUsername) {
+    check(userIdOrUsername, String);
     return ProfilesCollection.find({$or:[{userId:userIdOrUsername}, {username:userIdOrUsername}]});
 });
