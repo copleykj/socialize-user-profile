6a602c63f3b68a8093d4e89ceac84018078c407f
copleykj <copleykj@gmail.com>
2016-06-24 23:21:21 -0400

    update package.js

--- package.js
+++ package.js
@@ -1,19 +1,17 @@
 Package.describe({
     name: "socialize:user-profile",
     summary: "An extensible model for a users profile",
-    version: "0.2.1",
+    version: "1.0.0",
     git: "https://github.com/copleykj/socialize-user-profile.git"
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("1.0.2.1");
+    api.versionsFrom("1.3");
 
     api.use([
-        "socialize:user-model@0.1.7"
+        "socialize:user-model@1.0.0"
     ]);
 
-    api.addFiles(["common/profile-model.js", "common/user-extensions.js"]);
-    api.addFiles(["server/server.js"], "server");
-
-    api.export("Profile");
+    api.mainModule("server/server.js", "server");
+    api.addFiles("common/common.js");
 });
