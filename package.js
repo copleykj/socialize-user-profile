c8fc4a20a55acca1080ddfb84bf3dab61c01c2b8
Kelly Copley <copleykj@gmail.com>
2017-07-13 16:11:36 -0400

    depend on ecmascript package

--- package.js
+++ package.js
@@ -9,6 +9,7 @@ Package.onUse(function (api) {
     api.versionsFrom('1.3');
 
     api.use([
+        'ecmascript',
         'socialize:base-model@1.0.0',
         'socialize:user-model@1.0.0',
     ]);
