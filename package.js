08133fe0083a59c569d03cd25593086a5bbc672e
Kelly Copley <copleykj@gmail.com>
2017-07-13 16:08:50 -0400

    depend on base-model explicitly

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
-    name: "socialize:user-profile",
-    summary: "An extensible model for a users profile",
-    version: "1.0.0",
-    git: "https://github.com/copleykj/socialize-user-profile.git"
+    name: 'socialize:user-profile',
+    summary: 'An extensible model for a users profile',
+    version: '1.0.0',
+    git: 'https://github.com/copleykj/socialize-user-profile.git',
 });
 
-Package.onUse(function(api) {
-    api.versionsFrom("1.3");
+Package.onUse(function (api) {
+    api.versionsFrom('1.3');
 
     api.use([
-        "socialize:user-model@1.0.0"
+        'socialize:base-model@1.0.0',
+        'socialize:user-model@1.0.0',
     ]);
 
-    api.mainModule("server/server.js", "server");
-    api.mainModule("common/common.js");
+    api.mainModule('server/server.js', 'server');
+    api.mainModule('common/common.js');
 });
