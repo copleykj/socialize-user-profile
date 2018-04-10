96be2fa49be2a8b6d1e4b160cbaaaa3b375821dd
Kelly Copley <copleykj@gmail.com>
2018-04-10 12:04:35 -0400

    fix incorrect paths not caught by eslint

--- entry-rn.js
+++ entry-rn.js
@@ -5,8 +5,8 @@ import { ServerTime } from '@socialize/server-time';
 import { User } from '@socialize/user-model';
 /* eslint-disable import/no-unresolved */
 
-import extendUser from './user-extensions';
-import construct from './profile-model.js';
+import extendUser from './common/user-extensions';
+import construct from './common/profile-model.js';
 
 const { Profile, ProfilesCollection } = construct({ Meteor, Mongo, LinkableModel, LinkParent, ServerTime });
 extendUser(User);
