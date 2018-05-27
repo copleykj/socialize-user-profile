af8fbea785724af45436aefe8236957d6b57b0dd
Kelly Copley <copleykj@gmail.com>
2018-05-26 22:59:29 -0400

    update deps

--- package.js
+++ package.js
@@ -9,8 +9,8 @@ Package.describe({
 Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
-    api.use('socialize:user-blocking@1.0.0');
-    api.use('socialize:friendships@1.0.0', { weak: true });
+    api.use('socialize:user-blocking@1.0.1');
+    api.use('socialize:friendships@1.0.2', { weak: true });
 
     api.mainModule('server/server.js', 'server');
     api.mainModule('common/common.js', 'client');
