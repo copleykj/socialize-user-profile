5ab664b6d0507f34771391503be9bd2d8121393b
Kelly Copley <copleykj@gmail.com>
2018-05-27 11:49:18 -0400

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:user-profile',
     summary: 'An extensible model for a users profile',
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/copleykj/socialize-user-profile.git',
 });
 
