f2cc27a84e2981c0643d73ccdea43236e4e73082
Kelly Copley <copleykj@gmail.com>
2020-07-31 08:09:33 -0400

    update deps

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:user-profile',
     summary: 'An extensible model for a users profile',
-    version: '1.0.2',
+    version: '1.0.3',
     git: 'https://github.com/copleykj/socialize-user-profile.git',
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
     api.use('socialize:user-blocking@1.0.1');
-    api.use('socialize:friendships@1.0.2', { weak: true });
+    api.use('socialize:friendships@1.1.0', { weak: true });
 
     api.mainModule('server/server.js', 'server');
     api.mainModule('common/common.js', 'client');
